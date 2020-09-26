8370eebbe52a65a9760a3ded4b75207d87c111f3
LaukeyMwaura <laukeymwaura@gmail.com>
2020-09-26 16:46:22 +0300

    Updates

---
+++ precache-manifest.7f0f7fc369da41bc4538a603f1d36e9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b85b96e6059b9710ebe018efeb3a79f",
+    "url": "/my-amazon/index.html"
+  },
+  {
+    "revision": "612de87a37e0460823ef",
+    "url": "/my-amazon/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "2f874fd58aeb8dff33f0",
+    "url": "/my-amazon/static/css/main.1d62e718.chunk.css"
+  },
+  {
+    "revision": "612de87a37e0460823ef",
+    "url": "/my-amazon/static/js/2.ff3c6721.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/my-amazon/static/js/2.ff3c6721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f874fd58aeb8dff33f0",
+    "url": "/my-amazon/static/js/main.d3997168.chunk.js"
+  },
+  {
+    "revision": "cda2fb3a7f895eaeb585",
+    "url": "/my-amazon/static/js/runtime-main.f3e8c25a.js"
+  }
+]);
